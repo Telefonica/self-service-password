cf49a34f83b8b8e3598398f0135b28f8799bbe7c
Daniel Mayor <daniel.mayor@amaris.com>
2019-03-14 13:12:45 +0100

    New UI

--- index.php
+++ index.php
@@ -178,22 +178,12 @@ $mailer->LE            = $mail_newline;
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta name="author" content="LDAP Tool Box" />
-    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css" />
-    <link rel="stylesheet" type="text/css" href="css/bootstrap-theme.min.css" />
-    <link rel="stylesheet" type="text/css" href="css/font-awesome.min.css" />
-    <link rel="stylesheet" type="text/css" href="css/self-service-password.css" />
-    <link href="images/favicon.ico" rel="icon" type="image/x-icon" />
-    <link href="images/favicon.ico" rel="shortcut icon" />
-<?php if (isset($background_image)) { ?>
-     <style>
-       html, body {
-         background: url("<?php echo $background_image ?>") no-repeat center fixed;
-         background-size: cover;
-       }
-  </style>
-<?php } ?>
+    <link href="css/site-bf872215389ce9bdfed0fb044f437dc9-801413a6.css" rel="stylesheet" type="text/css" />
+    <link href="css/site-9931e18bd886c1cc8ea8988d1b3b38c4-72bb4cf0.css" rel="stylesheet" type="text/css" />
+    <link href="images/favicon-32x32-e02d4900.png" rel="shortcut icon" type="image/png" />
 </head>
-<body>
+
+<body class="index">
 
 <div class="container">
 
@@ -202,20 +192,12 @@ $mailer->LE            = $mail_newline;
 
 <?php if ( $show_menu ) {
     include("menu.php");
-} else { ?>
-<div class="title alert alert-success text-center"><h1><?php echo $messages["title"]; ?></h1></div>
-<?php } ?>
-
-<?php if ( $logo ) { ?>
-<a href="index.php" alt="Home">
-<img src="<?php echo $logo; ?>" alt="Logo" class="logo img-responsive center-block" />
-</a>
-<?php } ?>
+}?>
 
 <?php
     if ( count($dependency_check_results) > 0 ) {
         foreach($dependency_check_results as $result) {
-            ?>
+?>
             <div class="result alert alert-<?php echo get_criticity($result) ?>">
                 <p><i class="fa fa-fw <?php echo get_fa_class($result) ?>" aria-hidden="true"></i> <?php echo $messages[$result]; ?></p>
             </div>
